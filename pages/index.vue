5d865a7e1299d6d71335432db18ac28fcd8df3f1
Michaela Havlíková <Michelle.h@seznam.cz>
2024-10-30 23:59:10 +0100

    Add tabs component from shadcn and use it in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <script setup>
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
 const loading = ref(false)
+const list = [
+  {
+    title: "Today",
+    component: "<div>Today</div>"
+  }, {
+    title: "Week",
+    component: "<div>Week</div>"
+  }, {
+    title: "Month",
+    component: "<div>Month</div>"
+  }, {
+    title: "Year",
+    component: "<div>Year</div>"
+  }
+]
 </script>
 
 <template>
@@ -12,12 +29,22 @@ const loading = ref(false)
       <div class="w-[120px] h-[36px] bg-neutral-200"></div>
     </header>
     <main class="grid gap-4">
-      <div class="flex items-center gap-4">
+      <Tabs default-value="Today" class="w-[400px]">
+        <TabsList>
+          <TabsTrigger v-for="(item, index) in list" :key="index" :value="item.title">
+            {{ item.title }}
+          </TabsTrigger>          
+        </TabsList>
+        <TabsContent v-for="(item, index) in list" :key="index" :value="item.title">
+          {{ item.component }}
+        </TabsContent>
+      </Tabs>
+      <!-- <div class="flex items-center gap-4">
         <div v-for="(item, index) in 3" :key="index" class="w-[120px] h-[36px] bg-neutral-200"></div>
       </div>
       <section>
         <div class="w-full h-[360px] bg-neutral-200"></div>
-      </section>
+      </section> -->
     </main>
     <footer class="flex items-center gap-4">
       <div v-for="(item, index) in 3" :key="index" class="w-full h-[260px] bg-neutral-200"></div>
