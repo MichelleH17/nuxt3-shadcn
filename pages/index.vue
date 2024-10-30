75f6f107adf7f949c9e09ac2e688b2dbe9b65067
Michaela Havlíková <Michelle.h@seznam.cz>
2024-10-30 19:56:01 +0100

    Edit content of the index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
-<template>
-  <div>
-    <p>Main page</p>
-  </div>
-</template>
-
 <script setup>
 const loading = ref(false)
 </script>
+
+<template>
+  <div>
+    <header>
+      This is my header
+    </header>
+    <main>
+      This is my main
+      <section>
+        This is my section
+      </section>
+    </main>
+    <footer>
+      This is my footer
+    </footer>
+  </div>
+</template>
